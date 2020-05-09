ca9f52ac547cc482352317f24db0e15283cbc7b0
Catherine Price <catprice444@gmail.com>
2020-05-09 15:32:51 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,14 +71,14 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  holiday_hash.map do |season, holiday|
+    holiday.map do |event, supply|
+      event if supply.include?("BBQ")
+    end
+  end.flatten.compact
   # holiday_hash.map do |season, holiday|
-  #   holiday.map do |event, supply|
+  #   holiday.map do |holiday, supply|
   #     holiday if supply.include?("BBQ")
   #   end
   # end.flatten.compact
-  holiday_hash.map do |season, holiday|
-    holiday.map do |holiday, supply|
-      holiday if supply.include?("BBQ")
-    end
-  end.flatten.compact
 end
